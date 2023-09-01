e3a97e20769a119a8f0bec7e997b29a1444c521f
hoangnguyen1017 <142413653+hoangnguyen1017@users.noreply.github.com>
2023-09-01 15:39:25 +0700

    update user

--- menu.py
+++ menu.py
@@ -1,207 +1,180 @@
-import lol
-class Champion:
-    def __init__(self, id="", name="", role="", health="", attack="", defense="", attackSpeed=""):
-        self.id = id
-        self.name = name
-        self.role = role
-        self.health = health
-        self.attack = attack
-        self.defense = defense
-        self.attackSpeed = attackSpeed
-
-
-    def __str__(self):
-        return self.id + ","+self.name+","+self.role+"," + self.health+"," + self.attack+","+self.defense+ ","+ self.attackSpeed+","
-
-def isIDvalid(input):
-    if input.isdigit():
-        return input
-    else:
-        print("The ID format must be a number")
-
-def isNamevalid(input):
-    if input.isalpha():
-        return input
-    else:
-        print("The Name format must be character")
-def isRolevalid(input):
-    if input.isalpha():
-        return input
-    else:
-        print("The Role format must be character")
-def isHealthvalid(input):
-    if input.isdigit():
-        return input
-    else:
-        print("The Heal format must be a number")
-def isAttackvalid(input):
-    if input.isdigit():
-        return input
-    else:
-        print("The Attack format must be a number")
-def isDefensevalid(input):
-    if input.isdigit():
-        return input
-    else:
-        print("The Defense format must be a number")
-def isAttackSpeed(input):
-    if ('.' in input):
-        f, l = input.split('.')
-        if f.isdigit() and l.isdigit():
-            return input
-    else:
-        print("The Attack Speed format must be a decimal")
-def champion_outcome(champions):
-    battle_result = []
-
-    for champion1 in champions:
-        for champion2 in champions:
-            if champion1 != champion2:
-                effective_damage_champ1 = (int(champion1.attack) * 3 - int(champion2.defense) * 2) * float(champion1.attackSpeed)
-                hits_required_champ1 = int(champion2.health) / int(effective_damage_champ1)
-
-                effective_damage_champ2 = (int(champion2.attack) * 3 - int(champion1.defense) * 2) * float(champion2.attackSpeed)
-                hits_required_champ2 = int(champion1.health) / int(effective_damage_champ2)
-
-                if int(hits_required_champ1) < int(hits_required_champ2):
-                    winner = [champion1.name]
-                else:
-                    winner = [champion2.name]
-
-                battle_result.append((champion1.name, champion2.name, winner))
-
-    return battle_result
-
+import lol_SQL
+import util
+import champion
+import user_SQL
 while True:
-    print("**********MENU**********")
-    print("1:Add champion")
-    print("2:Display the list of champion")
-    print("3:Delete champion")
-    print("4:Delete champion base on name")
-    print("5:Compare two champions (based on health, attack and defense)")
-    print("6:Update champion")
-    print("7:Display the champion have same role")
-    print("8:Search the champion (name)")
-    print("9:Open file infor champion")
-    print("10: Battle result")
-    print("11: Exit")
-    action = int(input('choose a option[1..11]: '))
-    if action > 11 or action <= 0:
+    print("**********WELCOME TO LOL CHAMPION**********")
+    print("1: Create Account")
+    print("2: Login Account")
+    print("3: Exit")
+    choose = int(input("choose a option 1 , 2, 3: "))
+    if choose > 3 or choose <= 0:
         continue
 
-    if action == 11:
+    if choose == 3:
         print('Good bye!')
         quit(0)
+    elif choose == 1:
+        id = util.isIdUserValid(input("Enter ID: "))
+        name1 = util.isNameUserValid(input("Enter First Name : "))
+        name2 = util.isNameUserValid(input("Enter Surname : "))
+        password = util.isPassValid(input("Enter Password (> 10 characters): "))
+        if id and name1 and name2 and password:
+            name = str(name1) + " " + str(name2)
+            user_SQL.addUser(id, name, password)
 
-    elif action == 1:
-        id = isIDvalid(input("Enter ID champion: "))
-        name = isNamevalid(input("Enter Name champion: "))
-        role = isRolevalid(input("Enter Role champion: "))
-        health = isHealthvalid(input("Enter Health champion: "))
-        attack = isAttackvalid(input("Enter Attack champion: "))
-        defense = isDefensevalid(input("Enter Defense champion: "))
-        attack_speed = isAttackSpeed(input("Enter Attack Speed champion: "))
-        if id and name and role and health and attack and defense:
-            lol.addChampionAttributesIntoTable(id, name, role, health, attack, defense,attack_speed)
-        else:
-            print("Can not add champion (Syntax error)")
-
-
-    elif action == 2:
-        lol.displayTable()
-    elif action == 3:
-        id = isIDvalid(input("Enter ID champion to delete: "))
-        if id:
-            lol.deleteVaribleIntoTable(id)
-        else:
-            print("Can not delete champion (Syntax error)")
-
-
-    elif action == 4:
-        name = isNamevalid(input("Enter Name champion to delete: "))
-        if name:
-            lol.deleteChampion(name)
-        else:
-            print("Can not delete champion (Syntax error)")
-
-
-    elif action == 5:
-        name1 = isNamevalid(input("Enter Name champion 1: "))
-        name2 = isNamevalid(input("Enter Name champion 2: "))
-        if name1 and name2:
-            lol.compareChampions(name1,name2)
 
         else:
-            print("Can not find champion (Syntax error)")
+            print("Can not create account (Syntax error) ")
+    elif choose == 2:
+        name = util.isNameloginAccountValid(input("Please enter your account: "))
+        password = util.isPassValid(input("Please enter a password: "))
+        check = user_SQL.validUser(name,password)
+        if check is False:
+            print("Wrong account or password. Please check again")
 
-
-    elif action == 6:
-        id = isIDvalid(input("Enter ID champion to update: "))
-        name = isNamevalid(input("Enter Name champion: "))
-        role = isRolevalid(input("Enter Role champion: "))
-        health = isHealthvalid(input("Enter Health champion: "))
-        attack = isAttackvalid(input("Enter Attack champion: "))
-        defense = isDefensevalid(input("Enter Defense champion: "))
-        attack_speed = isAttackSpeed(input("Enter Attack Speed champion: "))
-        if id and name and role and health and attack and defense and attack_speed:
-            lol.updateChampion(id,name,role,health,attack,defense,attack_speed)
         else:
-            print("Can not update champion (Syntax error)")
+            print("Logged in successfully")
+
+            while True:
+                print("**********MENU**********")
+                print("1:Add champion")
+                print("2:Display the list of champion")
+                print("3:Delete champion")
+                print("4:Delete champion base on name")
+                print("5:Compare two champions (based on health, attack and defense)")
+                print("6:Update champion")
+                print("7:Display the champion have same role")
+                print("8:Search the champion (name)")
+                print("9:Open file infor champion")
+                print("10: Battle result")
+                print("11: Exit")
+                action = int(input('choose a option[1..11]: '))
+                if action > 11 or action <= 0:
+                    continue
+
+                if action == 11:
+                    print('Good bye!')
+                    quit(0)
+
+                elif action == 1:
+                    id = util.isIDvalid(input("Enter ID champion: "))
+                    name = util.isNamevalid(input("Enter Name champion: "))
+                    role = util.isRolevalid(input("Enter Role champion: "))
+                    health = util.isHealthvalid(input("Enter Health champion: "))
+                    attack = util.isAttackvalid(input("Enter Attack champion: "))
+                    defense = util.isDefensevalid(input("Enter Defense champion: "))
+                    attack_speed = util.isAttackSpeed(input("Enter Attack Speed champion: "))
+                    if id and name and role and health and attack and defense:
+                        lol_SQL.addChampionAttributesIntoTable(id, name, role, health, attack, defense, attack_speed)
+                    else:
+                        print("Can not add champion (Syntax error)")
 
 
-    elif action == 7:
-        role = isRolevalid(input("Enter Role champion: "))
-        if role:
-            lol.displayRoleChampion(role)
-        else:
-            print("Can not find champion (Syntax error)")
+                elif action == 2:
+                    lol_SQL.displayTable()
+                elif action == 3:
+                    id = util.isIDvalid(input("Enter ID champion to delete: "))
+                    if id:
+                        lol_SQL.deleteVaribleIntoTable(id)
+                    else:
+                        print("Can not delete champion (Syntax error)")
 
 
-    elif action == 8:
-        name = isNamevalid(input("Enter Name champion: "))
-        if name:
-            lol.searchChampion(name)
-        else:
-            print("Can not find champion (Syntax error)")
-    #update action 9
-    elif action == 9:
-
-        file_name = input('Enter a file: ')
-        try:
-            with open(file_name, "r") as csv_file:
-                reader = csv_file.readlines()
-                for line in reader:
-                    attr = line.strip().split(',')
-                    champion_id = attr[0]
-                    champion_name = attr[1]
-                    champion_role = attr[2]
-                    champion_health = attr[3]
-                    champion_attack = attr[4]
-                    champion_defense = attr[5]
-                    champion_attackSpeed = attr[6]
-                    lol.addChampionAttributesIntoTable(champion_id, champion_name, champion_role, champion_health,
-                                                       champion_attack, champion_defense, champion_attackSpeed)
+                elif action == 4:
+                    name = util.isNamevalid(input("Enter Name champion to delete: "))
+                    if name:
+                        lol_SQL.deleteChampion(name)
+                    else:
+                        print("Can not delete champion (Syntax error)")
 
 
-        except FileNotFoundError:
-            print('File not found')
-   
-    elif action == 10:
-        champions = []
-        view = "select * from Champion"
-        records = lol.fetch_all(view)
-        for record in records:
-            id = record[0]
-            name = record[1]
-            role = record[2]
-            health = record[3]
-            attack = record[4]
-            defense = record[5]
-            attack_speed = record[6]
-            champion = Champion(id, name, role, health, attack, defense, attack_speed)
-            champions.append(champion)
+                elif action == 5:
+                    name1 = util.isNamevalid(input("Enter Name champion 1: "))
+                    name2 = util.isNamevalid(input("Enter Name champion 2: "))
 
-        result = champion_outcome(champions)
-        print(result)
+                    if name1 and name2:
+                        lol_SQL.compareChampions(name1, name2)
+
+                    else:
+                        print("Can not find champion (Syntax error)")
+
+
+                elif action == 6:
+                    id = util.isIDvalid(input("Enter ID champion to update: "))
+                    name = util.isNamevalid(input("Enter Name champion: "))
+                    role = util.isRolevalid(input("Enter Role champion: "))
+                    health = util.isHealthvalid(input("Enter Health champion: "))
+                    attack = util.isAttackvalid(input("Enter Attack champion: "))
+                    defense = util.isDefensevalid(input("Enter Defense champion: "))
+                    attack_speed = util.isAttackSpeed(input("Enter Attack Speed champion: "))
+                    if id and name and role and health and attack and defense and attack_speed:
+                        lol_SQL.updateChampion(id, name, role, health, attack, defense, attack_speed)
+                    else:
+                        print("Can not update champion (Syntax error)")
+
+
+                elif action == 7:
+                    role = util.isRolevalid(input("Enter Role champion: "))
+                    if role:
+                        lol_SQL.displayRoleChampion(role)
+                    else:
+                        print("Can not find champion (Syntax error)")
+
+
+                elif action == 8:
+                    name = util.isNamevalid(input("Enter Name champion: "))
+                    if name:
+                        lol_SQL.searchChampion(name)
+                    else:
+                        print("Can not find champion (Syntax error)")
+                # update action 9
+                elif action == 9:
+
+                    file_name = input('Enter a file: ')
+                    try:
+                        with open(file_name, "r") as csv_file:
+                            reader = csv_file.readlines()
+                            for line in reader:
+                                attr = line.strip().split(',')
+                                champion_id = attr[0]
+                                champion_name = attr[1]
+                                champion_role = attr[2]
+                                champion_health = attr[3]
+                                champion_attack = attr[4]
+                                champion_defense = attr[5]
+                                champion_attackSpeed = attr[6]
+                                lol_SQL.addChampionAttributesIntoTable(champion_id, champion_name, champion_role,
+                                                                       champion_health,
+                                                                       champion_attack, champion_defense,
+                                                                       champion_attackSpeed)
+
+
+                    except FileNotFoundError:
+                        print('File not found')
+
+                elif action == 10:
+                    champions = []
+                    view = "select * from Champion"
+                    records = lol_SQL.fetch_all(view)
+                    for record in records:
+                        id = record[0]
+                        name = record[1]
+                        role = record[2]
+                        health = record[3]
+                        attack = record[4]
+                        defense = record[5]
+                        attack_speed = record[6]
+                        champion = champion.Champion(id, name, role, health, attack, defense, attack_speed)
+                        champions.append(champion)
+
+                    result = util.champion_outcome(champions)
+                    print(result)
+
+
+
+
+
+
 
 
--- util.py
+++ util.py
@@ -1,47 +1,87 @@
-# Objectives of champion_outcome function
-# 1. Calculate the effective damage one champion would deal to another.
-# 2. Compute the number of hits required for one champion to defeat another.
-# 3. Determine the winner of each battle by comparing the number of hits each champion requires to defeat the other.
-# 4. Return a list of battle outcomes.
+def isIDvalid(input):
+    if input.isdigit():
+        return input
+    else:
+        print("The ID format must be a number")
 
-# Create a function name champion_outcome
-# function champion_outcome will take the dataframe as an parameter
-# in function champion_outcome we will compute the champion outcome as follow
-# Compute the effective damage by champ1 to champ2 considering attack_power, defense_power, and attack_speed.
-# Calculate the number of hits required by champ1 to defeat champ2 using the effective damage and health of champ2.
-# We'll use the formula:
-# Effective Damage = (champ1.attack_power * 2 - champ2.defense_power * 3) * champ1.attack_speed
-# Hits Required = champ2.health / Effective damge
-# If Hits Required for champ1 is less than that for champ2, then champ1 is the winner.
+def isNamevalid(input):
+    if input.isalpha():
+        return input
+    else:
+        print("The Name format must be character")
+def isRolevalid(input):
+    if input.isalpha():
+        return input
+    else:
+        print("The Role format must be character")
+def isHealthvalid(input):
+    if input.isdigit():
+        return input
+    else:
+        print("The Heal format must be a number")
+def isAttackvalid(input):
+    if input.isdigit():
+        return input
+    else:
+        print("The Attack format must be a number")
+def isDefensevalid(input):
+    if input.isdigit():
+        return input
+    else:
+        print("The Defense format must be a number")
+def isAttackSpeed(input):
+    if ('.' in input):
+        f, l = input.split('.')
+        if f.isdigit() and l.isdigit():
+            return input
+    else:
+        print("The Attack Speed format must be a decimal")
 
-# Pseudocode 
-# Function champion_outcome (Input: List of Champions with their stats)
-    
-#     Initialize an empty list called "battle_results"
+def isIdUserValid(input):
+    if input.isdigit():
+        return input
+    else:
+        print("The ID format must be a number")
 
-#     For each "champion1" in the list of Champions:
-#         For each "champion2" in the list of Champions:
-            
-#             If champion1 is not equal to champion2:
-                
-#                 // Calculate the effective damage of champion1 against champion2
-#                 effective_damage_champ1 = (champion1's attack_power * 2 - champion2's defense_power * 3) * champion1's attack_speed
-                
-#                 // Calculate the number of hits champion1 needs to defeat champion2
-#                 hits_required_champ1 = champion2's health divided by effective_damage_champ1
+def isNameUserValid(input):
+    if input.isalpha():
+        return input
+    else:
+        print("The Name format must be character")
 
-#                 // Calculate the effective damage of champion2 against champion1
-#                 effective_damage_champ2 = (champion2's attack_power * 2 - champion1's defense_power * 3) * champion2's attack_speed
-                
-#                 // Calculate the number of hits champion2 needs to defeat champion1
-#                 hits_required_champ2 = champion1's health divided by effective_damage_champ2
-                
-#                 // Determine the outcome based on who needs fewer hits
-#                 If hits_required_champ1 is less than hits_required_champ2:
-#                     champion1 is the winner
-#                     Append (champion1's name, champion2's name, and the result) to "battle_results"
-#                 Else:
-#                     champion2 is the winner
-#                     Append (champion1's name, champion2's name, and the result) to "battle_results"
-    
-#     Return "battle_results"
+
+def isNameloginAccountValid(input):
+    if (' ' in input):
+        f, l = input.split(" ", 1)
+        if f.isalpha and l.isalpha():
+            return input
+    else:
+        print("The Login account format must be First_name Surname")
+
+
+def isPassValid(input):
+    if len(input) > 10:
+        return input
+    else:
+        print("The Password format must be greater than 10 characters")
+
+def champion_outcome(champions):
+    battle_result = []
+
+    for champion1 in champions:
+        for champion2 in champions:
+            if champion1 != champion2:
+                effective_damage_champ1 = (int(champion1.attack) * 3 - int(champion2.defense) * 2) * float(champion1.attackSpeed)
+                hits_required_champ1 = int(champion2.health) / int(effective_damage_champ1)
+
+                effective_damage_champ2 = (int(champion2.attack) * 3 - int(champion1.defense) * 2) * float(champion2.attackSpeed)
+                hits_required_champ2 = int(champion1.health) / int(effective_damage_champ2)
+
+                if int(hits_required_champ1) < int(hits_required_champ2):
+                    winner = [champion1.name]
+                else:
+                    winner = [champion2.name]
+
+                battle_result.append((champion1.name, champion2.name, winner))
+
+    return battle_result
